98765a3e663f5559894c6ba7292a10111d6abb34
Mont3iro68 <manueljoao1996@hotmail.com>
2017-11-24 20:12:35 +0000

    query 2

---
+++ Procura_animaisMedicamento.sql
@@ -0,0 +1,24 @@
+USE `4patas`;
+-- Procura dos animais a quais foram prescrito o medicamento X
+ 
+DELIMITER $$
+CREATE PROCEDURE FindAnimalMedicamento(IN medicamento VARCHAR(64))
+
+Begin
+SELECT * FROM Animal AS A
+	INNER JOIN Consulta AS C
+    ON C.Animal_Id_Animal = A.id_Animal
+		INNER JOIN Diagnóstico AS D
+        ON D.Consulta_id_consulta = C.id_consulta
+			INNER JOIN Medicamentos AS M
+            ON M.Diagnóstico_id_diag = D.id_diag
+				WHERE M.nome = medicamento;
+
+END $$
+
+
+SET @medicamento = 'Analgésico';
+
+CALL FindAnimalMedicamento(@medicamento);
+
+DROP PROCEDURE FindAnimalMedicamento;
